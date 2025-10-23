91e023561c43af0bd6c0d1bb1527cf10481efe5e
EECKHOUTTE Lucas <lucas1.eeckhoutte@epitech.eu>
2025-10-23 18:13:03 +0200

    [+] add hello, ping, invite, github

--- client.py
+++ client.py
@@ -10,11 +10,11 @@ config = loads(Path("config.json").read_text())
 
 TOKEN = config["token"]
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Je suis connecté")
+    print(f"Je suis connecté avec {bot.user}")
     await bot.change_presence(activity=discord.Game("42"), status=discord.Status.online)
 
 @bot.event
@@ -27,5 +27,11 @@ async def on_message(message):
     
     if message.content.startswith('$ping'):
         await message.channel.send('pong')
+    
+    if message.content.startswith('$invite'):
+        await message.channel.send('Salut ! Mon invitation est :' '\n ''https://discord.com/oauth2/authorize?client_id=904652429381992459')
+    
+    if message.content.startswith('$github'):
+        await message.channel.send('Salut ! Libre à toi de venir m améliorer ! voici mon lien :' '\n' 'https://github.com/Tadomika-Ari/M4RV1N-Bot')
 
 bot.run(TOKEN)
