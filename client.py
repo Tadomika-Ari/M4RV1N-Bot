d0384502bea733e503c43d70af3354fd11ebcc7f
Peugeot205GTI <utilisateur01gunner@gmail.com>
2025-10-24 10:28:39 +0200

    jaaj

--- client.py
+++ client.py
@@ -1,37 +1,20 @@
 import discord
-from discord.ext import commands
+from discord import bot
 from json import loads
 from pathlib import Path
-
-intents = discord.Intents.default()
-intents.message_content = True
 
 config = loads(Path("config.json").read_text())
 
 TOKEN = config["token"]
 
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"Je suis connecté avec {bot.user}")
-    await bot.change_presence(activity=discord.Game("42"), status=discord.Status.online)
+    print(f"{bot.user} is ready and online!")
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    
-    if message.content.startswith('$ping'):
-        await message.channel.send('pong')
-    
-    if message.content.startswith('$invite'):
-        await message.channel.send('Salut ! Mon invitation est :' '\n ''https://discord.com/oauth2/authorize?client_id=904652429381992459')
-    
-    if message.content.startswith('$github'):
-        await message.channel.send('Salut ! Libre à toi de venir m améliorer ! voici mon lien :' '\n' 'https://github.com/Tadomika-Ari/M4RV1N-Bot')
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
 
 bot.run(TOKEN)
