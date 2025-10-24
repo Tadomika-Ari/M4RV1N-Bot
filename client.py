46c01708cd8b5f1711c80aa781a89107b529be7f
Peugeot205GTI <utilisateur01gunner@gmail.com>
2025-10-24 10:32:00 +0200

    jaaj

--- client.py
+++ client.py
@@ -1,22 +1,20 @@
 import discord
-from discord.ext import commands
+from discord import bot
 from json import loads
 from pathlib import Path
-
-intents = discord.Intents.default()
-intents.message_content = True
 
 config = loads(Path("config.json").read_text())
 
 TOKEN = config["token"]
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+bot = discord.Bot()
 
 @bot.event
 async def on_ready():
-    print(f"Je suis connecté")
-    await bot.change_presence(activity=discord.Game("42"), status=discord.Status.online)
+    print(f"{bot.user} is ready and online!")
 
-
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
 
 bot.run(TOKEN)
