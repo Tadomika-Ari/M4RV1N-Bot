6bcf71f69d25b0c66d2215046c289a1be80e8f7e
EECKHOUTTE Lucas <lucas1.eeckhoutte@epitech.eu>
2025-11-02 13:57:56 +0100

    [+] upd embed discord, [+] beginning create tournament, [+] beginning ban function, [+] beginning banlist, [+] function for command admin, [+] beginning function for check role for command ban or other

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ from json import loads
 from pathlib import Path
 
 from ressource.help import *
+from ressource.smash import *
 
 config = loads(Path("config.json").read_text())
 
@@ -33,12 +34,19 @@ async def ping(ctx):
    await ctx.respond(f"Pong ! {bot.latency} ms")
 
 @bot.slash_command(name="embed", description="Create a embed")
-async def tableau(interaction : discord.Interaction, title: str, description: str, time: str):
+async def tableau(interaction : discord.Interaction, title: str, description: str):
+   embed = discord.Embed(title=f"{title}", description=f"{description}")
+   embed.add_field(name="Ceci est un test :", value=f"Ceci aussi")
+   embed.set_footer(text=f"Créer par {interaction.user.name}")
+   await interaction.response.send_message(embed=embed)
+
+@bot.slash_command(name="tournoi", description="create a tournoi")
+async def add_tournoi(interaction : discord.Interaction, title: str, description: str, time: str):
    embed = discord.Embed(title=f"{title}", description=f"{description}")
    embed.add_field(name="Le tournoi commence dans :", value=f"Le trournoi commance dans <t:{time}:R>")
    embed.set_footer(text=f"Créer par {interaction.user.name}")
    await interaction.response.send_message(embed=embed)
-   
+
 @bot.slash_command(name="bio", description="The bio of Marvin")
 async def bio(ctx):
   print("Commande bio")
@@ -46,10 +54,26 @@ async def bio(ctx):
 
 @bot.slash_command(name="ban", description="its a ban !")
 async def ban(ctx: discord.ApplicationContext):
-    if ctx.author.guild_permissions.ban_members:
-      print("its a test")
+    allowed_role = ["Fondateur", "Modérateur"]
+    if any(role.name in allowed_role for role in ctx.author.roles):
+        await ctx.respond("test !")
     else:
-       print("pas la perm")
+        await ctx.respond("Tu n’as pas le rôle requis.", ephemeral=True)
+
+@bot.slash_command(name="banlist", description="Choisie le jeu et le nombre, je m'occupe du reste")
+async def tabban(interaction : discord.Interaction, name : str, nb: int):
+  print("ok1")
+  list = random_ban_character(["je", "jeededd", "keke", "hhehe"], nb)
+  embed = discord.Embed(title=f"La ban list de Smash", description="La list de ban pour cette partie est la suivante : ")
+  embed.add_field(name=name, value=list)
+  await interaction.response.send_message(embed=embed)
+
+@bot.slash_command(name="admin_only", description="Commande réservée aux admins")
+async def admin_only(ctx):
+    if "Fondateur" in [role.name for role in ctx.author.roles]:
+        await ctx.respond("Commande admin exécutée !")
+    else:
+        await ctx.respond("Tu n’as pas le rôle requis.", ephemeral=True)
 
 @bot.slash_command(name="help", description="The help function")
 async def help(interaction : discord.Interaction):
