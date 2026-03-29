ff52a7c4d54344d7d87779d25991d03b2d49e362
Alessandro Paris <alessandro1.paris@epitech.eu>
2026-03-29 20:29:33 +0200

    fix: zoneinfo paris europe

--- ressource/time.py
+++ ressource/time.py
@@ -1,6 +1,7 @@
 from time import *
 from datetime import timedelta
 import datetime
+from zoneinfo import ZoneInfo
 
 def time_to_timestamp(date_str):
     dt = datetime.datetime.strptime(date_str, "%d/%m/%Y")
@@ -8,7 +9,7 @@ def time_to_timestamp(date_str):
     return int(dt.timestamp())
 
 def get_time():
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(ZoneInfo("Europe/Paris"))
     date = now.strftime("%d/%m/%Y")
     time = now.strftime("%Hh%M")
     return "On est actuellement le " + date + " et il est " + time
